b2e76c26c17740913b3e08b71912ee25ade8fe3e
miawt <miawinthertamaki@gmail.com>
2025-07-01 22:41:04 -0700

    update homepage

---
+++ my-vibe-site/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-4">Fully Automated Luxury Gay Communism</h1>
+      <p className="text-lg text-gray-400">is this thing on?</p>
+    </main>
+  );
+}
